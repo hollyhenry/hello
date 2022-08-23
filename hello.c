6fea5ab3d86662d261b72ad2b55e487f613cfd41
hollyhenry <hollyhenry@unomaha.edu>
2022-08-23 20:17:14 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Holly Henry
+ * hollyhenry@unomaha.edu
+ * 2022/08/23
+ * hack 1 basic hello program and repository creation.
+ */
+
+  #include <stdlib.h>
+  #include <stdio.h>
+
+  int main (int argc, char **argv) {
+
+    printf("Holly Henry\n");
+
+  return 0;
+
+  }
